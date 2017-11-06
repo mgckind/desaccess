93971a7f591287cca2aebb2f3b070efd33ecd233
Matias Carrasco Kind <mgckind@gmail.com>
2017-11-06 00:53:55 -0600

    vulcanized dev version abc6b67

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """easyweb version"""
 
-version_tag = (1, 0, 0, 'dev-a163828')
+version_tag = (1, 0, 0, 'dev-abc6b67')
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
