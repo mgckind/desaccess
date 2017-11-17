f0e976cfc26675b6894d039badb595bf8cf8eefb
Matias Carrasco Kind <mgckind@gmail.com>
2017-11-17 17:26:15 -0600

    de version c32e53e

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """easyweb version"""
 
-version_tag = (1, 0, 0, 'dev-594ef47')
+version_tag = (1, 0, 0, 'dev-c32e53e')
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
