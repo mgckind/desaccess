5d96fcef111c594e5efb376fc05d05334d8abe32
Matias Carrasco Kind <mgckind@gmail.com>
2017-11-17 18:04:54 -0600

    version dev-fddec9d

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """easyweb version"""
 
-version_tag = (1, 0, 0, 'dev-478aff2')
+version_tag = (1, 0, 0, 'dev-fddec9d')
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
