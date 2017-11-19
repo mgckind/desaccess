172af3a25a124f0196828e2dc5a711417b6fcb72
Matias Carrasco Kind <mgckind@gmail.com>
2017-11-19 10:45:12 -0600

    version dev-fd72b1e

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """easyweb version"""
 
-version_tag = (1, 0, 0, 'dev-2ee10e0')
+version_tag = (1, 0, 0, 'dev-fd72b1e')
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
