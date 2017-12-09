d750c2211ad9deae156e524ead78c7bbfbbc0fda
Matias Carrasco Kind <mgckind@gmail.com>
2017-12-09 16:53:56 -0600

    version dev-51244c3

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """easyweb version"""
 
-version_tag = (1, 0, 0, 'dev-a8cc9d8')
+version_tag = (1, 0, 0, 'dev-51244c3')
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
