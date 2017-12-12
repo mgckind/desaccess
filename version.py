ca21e577c1269a8c633e8fccb15ae1c12439e67e
Matias Carrasco Kind <mgckind@gmail.com>
2017-12-11 18:14:12 -0600

    version dev-1928a5a

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """easyweb version"""
 
-version_tag = (1, 0, 0, 'dev-b84b8af')
+version_tag = (1, 0, 0, 'dev-1928a5a')
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
