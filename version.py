55e16a1685ce8d9321b8015826e5c7002af39716
Matias Carrasco Kind <mgckind@gmail.com>
2017-12-19 09:33:08 -0600

    version 1.0.0-dev-ee50b48

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """easyweb version"""
 
-version_tag = (1, 0, 0, 'dev-98fbae1')
+version_tag = (1, 0, 0, 'dev-ee50b48')
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
