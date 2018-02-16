44827fb6921ddb23d476db82b600b4fa9e44a645
Ruiqi Li <rli@signal.co>
2018-02-15 23:19:27 -0600

    Change DR1 -> DES; Add Epoch interface; Finished the implemetation and test on Coadd backend and doing test on Epoch backend; Debug for Epoch backend

--- cutout.py
+++ cutout.py
@@ -15,7 +15,21 @@ import datetime as dt
 import MySQLdb as mydb
 import yaml
 import ea_tasks
+import requests
+import re
+import config.descut as des
 
+from time import sleep
+
+def create_token():
+    req = requests.post('https://descut.cosmology.illinois.edu/api/token/',
+                        data={'username': des.USERNAME, 'password': des.PASSWORD}, verify=False)
+    return req
+
+class infoP(object):
+    def __init__(self, uu, pp):
+        self._uu=uu
+        self._pp=pp
 
 def dt_t(entry):
     t = dt.datetime.strptime(entry['time'], '%a %b %d %H:%M:%S %Y')
@@ -44,6 +58,8 @@ class FileHandler(BaseHandler):
         name = self.get_argument("name")
 
         stype = self.get_argument("submit_type")
+        values = self.get_argument("values")
+
         print('**************')
         print(xs, ys, 'sizes')
         print(stype, 'type')
@@ -80,19 +96,275 @@ class FileHandler(BaseHandler):
         os.system('mkdir -p '+folder2)
         now = datetime.datetime.now()
         input_csv = user_folder + jobid + '.csv'
-        run = ea_tasks.desthumb.apply_async(args=[input_csv, loc_user, lp.decode(),
-                                                  folder2, xs, ys, jobid, list_only,
-                                                  send_email, email], retry=True, task_id=jobid)
+
+        f = open(input_csv, 'r+')
+        ra = []
+        dec = []
+        count = 0
+        for line in f:
+            if count == 0:
+                count += 1
+                continue
+            count += 1
+            l = line.split(',')
+            r = float(l[0])
+            d = float(l[1])
+            ra.append(r)
+            dec.append(d)
+
+        f.close()
+
+
+        # create token
+        req = create_token()
+        print(req)
+        print(req.text)
+        response = req.json()
+        status = response['status']
+        if status == "error":
+            print("PASSWORD ERROR")
+            self.set_status(403)
+            self.flush()
+            self.finish()
+        token = response['token']
+
+
+        # create job query body
+        # create body of request
+        body = {
+            'token': token,  # required
+            'ra': str(ra),  # required
+            'dec': str(dec),  # required
+            'job_type': 'coadd',  # required 'coadd' or 'single'
+            'xsize': str(xs),  # optional (default : 1.0)
+            'ysize': str(ys),  # optional (default : 1.0)
+            'tag': 'Y3A1_COADD',
+        # optional for 'coadd' jobs (default: Y3A1_COADD, see Coadd Help page for more options)
+            'band': 'g,r,i',  # optional for 'single' epochs jobs (default: all bands)
+            'no_blacklist': 'false',
+        # optional for 'single' epochs jobs (default: 'false'). return or not blacklisted exposures
+            'list_only': list_only,  # optional (default : 'false') 'true': will not generate pngs (faster)
+            'comment': ''
+        }
+
+        if send_email:
+            body['email'] = email
+
+        # create body for files if needed
+        body_files = {'csvfile': open(input_csv, 'rb')}  # To load csv file as part of request
+        # To include files
+        req = requests.post('https://descut.cosmology.illinois.edu/api/jobs/', data=body, files=body_files, verify=False)
+        print(req)
+        print(req.text)
+        response = req.json()
+        status = response['status']
+        if status == 'error':
+            self.set_status(403)
+            self.flush()
+            self.finish()
+        jid =  response['job']
+        msg = response['message']
+        print (msg)
+        result = ''
+
+        while(1):
+            # get job result
+            url = "https://descut.cosmology.illinois.edu/api/jobs/?token={0}&jobid={1}".format(token, jid)
+            req = requests.get(url, verify=False)
+            print(req.text)
+            response = req.json()
+            status = response['status']
+            if status == 'ok':
+                result = response
+                break
+
+        # run = ea_tasks.desthumb.apply_async(args=[input_csv, loc_user, lp.decode(),
+        #                                           folder2, xs, ys, jobid, list_only,
+        #                                           send_email, email], retry=True, task_id=jobid)
         with open('config/mysqlconfig.yaml', 'r') as cfile:
             conf = yaml.load(cfile)['mysql']
         con = mydb.connect(**conf)
 
-        tup = tuple([loc_user, jobid, name, 'PENDING', now.strftime('%Y-%m-%d %H:%M:%S'),
-                     'cutout', '', '', '', -1])
+        tup = tuple([loc_user, jid, name, 'PENDING', now.strftime('%Y-%m-%d %H:%M:%S'),
+                     'coadd', '', '', '', -1])
         with con:
             cur = con.cursor()
             cur.execute("INSERT INTO Jobs VALUES {0}".format(tup))
         con.close()
+        self.set_status(200)
+        self.flush()
+        self.finish()
+
+
+
+
+class FileHandlerS(BaseHandler):
+    @tornado.web.asynchronous
+    @tornado.web.authenticated
+    def post(self):
+        print("REACH ME")
+        loc_user = self.get_secure_cookie("usera").decode('ascii').replace('\"','')
+        loc_passw = self.get_secure_cookie("userb").decode('ascii').replace('\"','')
+        user_folder = os.path.join(Settings.WORKDIR,loc_user)+'/'
+
+        cipher = AES.new(Settings.SKEY, AES.MODE_ECB)
+        lp = base64.b64encode(cipher.encrypt(loc_passw.rjust(32)))
+
+        xs = float(self.get_argument("xsize"))
+        ys = float(self.get_argument("ysize"))
+        list_only = self.get_argument("list_only") == 'true'
+        send_email = self.get_argument("send_email") == 'true'
+        noBlacklist = self.get_argument('noBlacklist') == 'true'
+        bands = self.get_argument('bands')
+        email = self.get_argument("email")
+        # comment = self.get_argument("comment")
+        stype = self.get_argument("submit_type")
+        name = self.get_argument("name")
+        if xs == 0.0 : xs=''
+        if ys == 0.0 : ys=''
+        print('**************')
+        print(xs,ys,'sizes')
+        print(stype,'type')
+        print(list_only,'list_only')
+        print(send_email,'send_email')
+        print(email,'email')
+        # print(comment,'comment')
+        print(bands, 'bands')
+        print(noBlacklist, 'noBlacklist')
+        print(name, 'name')
+        jobid = str(uuid.uuid4())
+        if stype=="manual":
+            values = self.get_argument("values")
+            print(values)
+            filename = user_folder+jobid+'.csv'
+            F=open(filename,'w')
+            F.write("RA,DEC\n")
+            F.write(values)
+            F.close()
+        if stype=="csvfile":
+            fileinfo = self.request.files["csvfile"][0]
+            fname = fileinfo['filename']
+            extn = os.path.splitext(fname)[1]
+            print(fname)
+            print(fileinfo['content_type'])
+            filename = user_folder+jobid+extn
+            with open(filename,'w') as F:
+                F.write(fileinfo['body'].decode('ascii'))
+        # print('**************')
+        # job_dir=user_folder+'results/'+jobid+'/'
+        # os.system('mkdir -p '+job_dir)
+        # infP=infoP(loc_user,loc_passw)
+        # now = datetime.datetime.now()
+        # tiid = loc_user+'__'+jobid+'_{'+now.strftime('%a %b %d %H:%M:%S %Y')+'}'
+
+        print('**************')
+        folder2 = user_folder + jobid + '/'
+        os.system('mkdir -p ' + folder2)
+        now = datetime.datetime.now()
+        input_csv = user_folder + jobid + '.csv'
+
+        f = open(input_csv, 'r+')
+        ra = []
+        dec = []
+        count = 0
+        for line in f:
+            if count == 0:
+                count += 1
+                continue
+            count += 1
+            l = line.split(',')
+            r = float(l[0])
+            d = float(l[1])
+            ra.append(r)
+            dec.append(d)
+
+        f.close()
+
+        # create token
+        req = create_token()
+        print(req)
+        print(req.text)
+        response = req.json()
+        status = response['status']
+        if status == "error":
+            print("PASSWORD ERROR")
+            self.set_status(403)
+            self.flush()
+            self.finish()
+        token = response['token']
+
+        # create job query body
+        # create body of request
+        body = {
+            'token': token,  # required
+            'ra': str(ra),  # required
+            'dec': str(dec),  # required
+            'job_type': 'single',  # required 'coadd' or 'single'
+            'xsize': str(xs),  # optional (default : 1.0)
+            'ysize': str(ys),  # optional (default : 1.0)
+            'tag': 'Y3A1_COADD',
+            # optional for 'coadd' jobs (default: Y3A1_COADD, see Coadd Help page for more options)
+            'band': bands,  # optional for 'single' epochs jobs (default: all bands)
+            'no_blacklist': noBlacklist,
+            # optional for 'single' epochs jobs (default: 'false'). return or not blacklisted exposures
+            'list_only': list_only,  # optional (default : 'false') 'true': will not generate pngs (faster)
+            'comment': ''
+        }
+
+        if send_email:
+            body['email'] = email
+
+        # create body for files if needed
+        body_files = {'csvfile': open(input_csv, 'rb')}  # To load csv file as part of request
+        # To include files
+        req = requests.post('https://descut.cosmology.illinois.edu/api/jobs/', data=body, files=body_files,
+                            verify=False)
+        print(req)
+        print(req.text)
+        response = req.json()
+        status = response['status']
+        if status == 'error':
+            self.set_status(403)
+            self.flush()
+            self.finish()
+        jid = response['job']
+        msg = response['message']
+        print (msg)
+        result = ''
+
+        while (1):
+            # get job result
+            url = "https://descut.cosmology.illinois.edu/api/jobs/?token={0}&jobid={1}".format(token, jid)
+            req = requests.get(url, verify=False)
+            print(req.text)
+            response = req.json()
+            status = response['status']
+            if status == 'ok':
+                result = response
+                break
+
+
+
+        # if send_email:
+        #     print('Sending email to %s' % email)
+        #     run=dtasks.mkcut.apply_async(args=[filename, loc_user, loc_passw, job_dir, xs, ys, bands, jobid, noBlacklist, tiid, list_only], \
+        #         task_id=tiid, link=dtasks.send_note.si(loc_user, tiid, toemail))
+        # else:
+        #     print('Not sending email')
+        #     run=dtasks.mkcut.apply_async(args=[filename, loc_user, loc_passw, job_dir, xs, ys, bands, jobid, noBlacklist, tiid, list_only], \
+        #         task_id=tiid)
+
+        with open('config/mysqlconfig.yaml', 'r') as cfile:
+            conf = yaml.load(cfile)['mysql']
+        con = mydb.connect(**conf)
+
+        tup = tuple([loc_user, jid, name, 'PENDING', now.strftime('%Y-%m-%d %H:%M:%S'),
+                     'epoch', '', '', '', -1])
+        with con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO Jobs VALUES {0}".format(tup))
+        con.close()
+
         self.set_status(200)
         self.flush()
         self.finish()
